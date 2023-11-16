133565513edf3893d8a8cea1a40fd73508a414bb
Rita Moura <105252658+rita-moura@users.noreply.github.com>
2023-11-16 16:35:21 -0300

    Feat: implementa componente de loading

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class LoadingComponent {
 
+  @Input()
+  loading = true;
 }
